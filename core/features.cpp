a0f7fc922c88e4be50dc26a82903f902ff5f84a0
Naseefabu <naseefbloomberg@gmail.com>
2022-07-22 10:22:23 +0530

    tapespeed

--- core/features.cpp
+++ core/features.cpp
@@ -1,1 +1,28 @@
 #include "features.hpp"
+
+TapeSpeedIndicator::TapeSpeedIndicator(int& lookback)
+{
+    lookback_period = lookback;
+}
+
+int TapeSpeedIndicator::get_tapespeed()
+{
+    remove();
+    return tapespeed;
+}
+
+void TapeSpeedIndicator::add(double &volume)
+{
+    if(volume > 0)
+    {
+        tapespeed++;
+        times.push_back(get_sec_timestamp(current_time()).count());
+    }
+}
+
+void TapeSpeedIndicator::remove()
+{
+    std::chrono::seconds lbt = get_sec_timestamp(current_time()).count() - lookback_period;
+    int temp=0;
+    int pop=0
+}
--- core/features.hpp
+++ core/features.hpp
@@ -2,19 +2,21 @@
 
 #include <iostream>
 #include <vector>
+#include "utils.hpp"
 
 // No. of Trades in a given time
 class TapeSpeedIndicator
 {
 public:
 
-    TapeSpeedIndicator(lookback);
+    TapeSpeedIndicator(int &lookback);
     void clear();
-    void add();
+    void add(double &volume);
     int get_tapespeed();
     void remove();
 
 private:
     int lookback_period = 60;
     int tapespeed = 0;
+    std::vector<float> times;
 };
